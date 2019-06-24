5df4edc8e260d059ffab07ab9de100d88318eaf8
Benoit Dekajoo <dekajoo@gmail.com>
2019-06-24 20:49:12 +0200

    Add some text to deployment script

--- build-and-run-prod.sh
+++ build-and-run-prod.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+echo ">>>>>>>> BUILDING FRONT";
 cd ./app/;
 npm install;
 npm run build;
+echo ">>>>>>>> BUILDING SERVER";
 cd ../server/;
 npm install;
 npm run build;
+echo ">>>>>>>> STARTING APP";
 PORT=3005 forever start index.js;
